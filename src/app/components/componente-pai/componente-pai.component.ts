3fb52e625332f5d895cade72f36d22db02768349
lp <lpcampanholi@gmail.com>
2025-01-10 15:27:27 -0300

    signals

--- src/app/components/componente-pai/componente-pai.component.ts
+++ src/app/components/componente-pai/componente-pai.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ComponenteFilhoComponent } from "../componente-filho/componente-filho.component";
 
 @Component({
@@ -9,9 +9,14 @@ import { ComponenteFilhoComponent } from "../componente-filho/componente-filho.c
   styleUrl: './componente-pai.component.css'
 })
 export class ComponentePaiComponent {
+  count = signal(0);
 
   logar(event: string) {
     console.log(event);
+  }
+
+  incrementar() {
+    this.count.update(value => value + 1);
   }
 
 }
